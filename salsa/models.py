413753446dc86b27b7901127526acc8fc53dae6f
Kevin Pozzils <kdp44@nau.edu>
2019-07-08 21:03:44 -0700

    building models

--- salsa/models.py
+++ salsa/models.py
@@ -1,4 +1,42 @@
 from django.db import models
 
 class Recipes(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=50)
+    tagLine = models.CharField(max_length=255)
+    recipeImages
+
+class Publication(models.Model):
+    title = models.CharField(max_length=30)
+
+    class Meta:
+        ordering = ('title',)
+
+    def __str__(self):
+        return self.title
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    publications = models.ManyToManyField(Publication)
+
+    class Meta:
+        ordering = ('headline',)
+
+    def __str__(self):
+        return self.headline
+
+    #   recipeName: '',
+    #   tagLine: '',
+    #   recipeImages: [],
+    #   availableIngredients: [],
+    #   availableCategories: [],
+    #   availableTags: [],
+    #   instructions: [],
+    #   ingredientsText: [],
+    #   category: '',
+    #   tags: [],
+    #   ingredients: [],
+    #   heat: '1',
+    #   yield: '',
+    #   difficulty: '1',
+    #   prepTime: '',
+    #   html: '',
